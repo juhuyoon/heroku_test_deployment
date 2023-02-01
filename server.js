f9d88691abf04aadfb1ba2c08eea202bab86df01
Junghoon Yoon <jyoon3@2u.com>
2023-02-01 09:41:58 -0500

    for demo

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
